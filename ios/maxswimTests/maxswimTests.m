2db924bf26047be90d0cf8c94dceded4fdb763d4
Denis <denisharapov@yandex.ru>
2021-02-09 00:08:59 +0700

    add forebase

--- ios/maxswimTests/maxswimTests.m
+++ ios/maxswimTests/maxswimTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface maxswimTests : XCTestCase
+@interface MaxSwimTests : XCTestCase
 
 @end
 
-@implementation maxswimTests
+@implementation MaxSwimTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
