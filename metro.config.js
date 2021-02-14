0d0f2f83eaff2f69b8a9166c85e8438bf9e8401d
Denis <denisharapov@yandex.ru>
2021-02-14 23:52:22 +0700

    add SVG loader

--- metro.config.js
+++ metro.config.js
@@ -4,14 +4,23 @@
  *
  * @format
  */
+const { getDefaultConfig } = require("metro-config");
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
       transform: {
         experimentalImportSupport: false,
         inlineRequires: false,
-      },
-    }),
-  },
-};
+      }
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
